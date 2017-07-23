3b61a8ba008bb0deea27ebc7d936c873a7a37897
andrew <andrew@devplace>
2017-07-23 15:19:46 -0300

    Initial commit

---
+++ src/br/com/andtankian/pp/PP.java
@@ -0,0 +1,16 @@
+package br.com.andtankian.pp;
+
+/**
+ *
+ * @author andrew
+ */
+public class PP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
